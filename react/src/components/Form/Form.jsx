2dea92cac76f731693be8b8289cc225210940de1
devandresantos <afds.santoss@gmail.com>
2023-04-04 18:02:54 -0300

    change button hover color

--- react/src/components/Form/Form.jsx
+++ react/src/components/Form/Form.jsx
@@ -4,6 +4,9 @@ import Button from '../Button/Button'
 import './Form.css'
 
 const Form = ({title, children, buttonFunction, buttonValue, lowLinkValue, lowLinkRef, buttonWidth}) => {
+    const BTN_HOVER_COLOR = '#28a4a4';
+    const BTN_COLOR = 'var(--secondary-color)';
+
     return (
             <div className='form-container'>
                  <h2 className='form-container-h2'>{title}</h2>
@@ -13,6 +16,9 @@ const Form = ({title, children, buttonFunction, buttonValue, lowLinkValue, lowLi
                             onClick={buttonFunction}
                             label={buttonValue}
                             width={buttonWidth}
+                            backgroundColor={BTN_COLOR}
+                            hoverColor={BTN_HOVER_COLOR}
+                            leaveColor={BTN_COLOR}
                         />
                         <div>
                              
