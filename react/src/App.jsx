19c33459544ad1b759fd1f8dba8043df95ffe387
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-04-09 10:53:13 -0300

    ajustes gerais

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,36 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.css'
 
-import Input from './components/Input/Input'
-import Form from './components/Form/Form'
-import InfoCardComponent from './components/InfoCardComponent/InfoCardComponent'
-
-import NavBar from './components/NavBar/NavBar'
-import Button from './components/Button/Button'
-import TermsAndConditions from './components/TermsAndConditions/TermsAndConditions'
 import BalancePage from './pages/balance_page'
 import Register from './pages/register'
 import TransferPage from './pages/transfer_page/TransferPage'
 import HomePage from './pages/home'
 import ImapayProvider from './context/imapayProvider'
-import CompleatTransfer from './pages/compleatTransfer/compleatTransfer'
+import CompletedTransfer from './pages/CompletedTransfer/CompletedTransfer'
 import LoginPage from './pages/login/LoginPage'
 import PasswordPage from './pages/password/PasswordPage'
 
 
 function App() {
-const alertButton = () => {
-  alert("123123");
-}
-
-const items = [<Button key="1" label="Cadastre-se" backgroundColor={"var(--secondary-color)"} hover={"var(--primary-color-light)"} click={() => {alert('1')}} width={""} />,
-              <Button key="1" label="Login" backgroundColor={"var(--primary-color-light)"} hover={"var(secondary-color)"} click={() => {alert('1')}} width={"100px"} />];
 
   return (
   
     <ImapayProvider>
       <Routes>
-        <Route path="/compleatTransfer" element={<CompleatTransfer />} />
+        <Route path="/completedTransfer" element={<CompletedTransfer />} />
         <Route path="/transfer" element={<TransferPage />} />
         <Route path="/balance" element={<BalancePage />} />
         <Route path="/register" element={<Register />} />
