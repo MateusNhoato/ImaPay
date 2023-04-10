57553dec0fc2688ea4675e5afd9f354ab92c8b36
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-04-10 00:01:11 -0300

    validation loginPage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import {React, useState, useRef} from 'react';
+import InfoCardComponent from '../../components/InfoCardComponent/InfoCardComponent';
 import Button from '../../components/Button/Button';
 import Form from '../../components/Form/Form';
 import Input from '../../components/Input/Input';
@@ -8,6 +8,11 @@ import NavBar from '../../components/NavBar/NavBar';
 import '../../App.css';
 
 const LoginPage = () => {
+
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword , setLoginPassword] = useState('');
+
+    const alertInvalidFields = useRef();
 
     const items = [<Button
         label={'Cadastre-se'}
@@ -30,10 +35,37 @@ const LoginPage = () => {
                     gradient={true}
                     // lowLinkRef = {colocar o link}
                     lowLinkValue ={'Esqueceu a senha?'}
+                    handleSubmit = {event => {
+                        event.preventDefault();
+                        if(!loginEmail){ 
+                            alertInvalidFields.current.innerText = 'Informe o email de login';
+                            return;
+                        }
+                        if(!loginPassword){
+                            alertInvalidFields.current.innerText = 'Informe a senha de login ';
+                            return;
+                        }
+                        alertInvalidFields.current.innerText = '';
+
+                    }}
                     
                 >
-                    <Input>E-mail</Input>
-                    <Input>Senha</Input>                               
+                    <p className='alert-invalid-fields' ref={alertInvalidFields}></p>
+                    <Input
+                        type={'email'}
+                        value={loginEmail}
+                        onChange={e => {
+                            setLoginEmail(e.target.value);
+                        }}
+                    >E-mail</Input>
+
+                    <Input
+                        type={'password'}
+                        value={loginPassword}
+                        onChange={e => {
+                            setLoginPassword(e.target.value);
+                        }}
+                    >Senha</Input>                             
                 </Form>              
                 
             </div>          
