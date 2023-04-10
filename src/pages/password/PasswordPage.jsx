30c4a187a5909b844ef9414c918501f502f04922
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-04-10 02:30:24 -0300

    validation register

--- src/pages/password/PasswordPage.jsx
+++ src/pages/password/PasswordPage.jsx
@@ -1,72 +1,80 @@
-import {React, useState, useRef} from 'react';
-import Button from '../../components/Button/Button';
-import Form from '../../components/Form/Form';
-import Input from '../../components/Input/Input';
-import NavBar from '../../components/NavBar/NavBar';
-import InfoCardComponent from '../../components/InfoCardComponent/InfoCardComponent';
+import { React, useState, useRef } from "react";
+import Button from "../../components/Button/Button";
+import Form from "../../components/Form/Form";
+import Input from "../../components/Input/Input";
+import NavBar from "../../components/NavBar/NavBar";
+import InfoCardComponent from "../../components/InfoCardComponent/InfoCardComponent";
 
-import '../../App.css';
-import './PasswordPage.css'
+import "../../App.css";
+import "./PasswordPage.css";
 const PasswordPage = () => {
+  const [recoveryEmail, setRecovertyEmail] = useState("");
 
-    const [recoveryEmail, setRecovertyEmail] = useState('');
+  const alertInvalidFields = useRef();
 
-    const alertInvalidFields = useRef();
-
-    const items = [<Button
-        label={'Cadastre-se'}
-        width={''}
-        backgroundColor={'var(--secondary-color)'}
-        click={() => {alert('Cadastre-se')}}
-        hoverColor={'var(--primary-color)'}
-        leaveColor={'var(--secondary-color)'}
+  const items = [
+    <Button
+      label={"Cadastre-se"}
+      width={""}
+      backgroundColor={"var(--secondary-color)"}
+      click={() => {
+        alert("Cadastre-se");
+      }}
+      hoverColor={"var(--primary-color)"}
+      leaveColor={"var(--secondary-color)"}
     />,
     <Button
-        label={'Login'}
-        width={''}
-        backgroundColor={'var(--primary-color)'}
-        click={() => {alert('Login')}}
-        hoverColor={'var(--secondary-color)'}
-        leaveColor={'var(--primary-color)'}
-    />];
+      label={"Login"}
+      width={""}
+      backgroundColor={"var(--primary-color)"}
+      click={() => {
+        alert("Login");
+      }}
+      hoverColor={"var(--secondary-color)"}
+      leaveColor={"var(--primary-color)"}
+    />,
+  ];
 
-    return (
-        <div>
-            <NavBar items={items} />
+  return (
+    <div>
+      <NavBar items={items} />
 
-            <div className='form-box'>
-                
-                <Form
-                    title={'Recuperação de senha'}                                      
-                    buttonValue={'Enviar'}
-                    buttonWidth={'90%'}
-                    gradient={true}
-                    handleSubmit = {event => {
-                        event.preventDefault();
-                        if(!recoveryEmail){ 
-                            alertInvalidFields.current.innerText = 'Tipo da conta não informado';
-                            return;
-                        }
-                        alertInvalidFields.current.innerText = '';
-
-                    }}
-
-                >
-                    <p className='alert-invalid-fields' ref={alertInvalidFields}></p>
-                    <h3 className='form-h3'>Forneça o E-mail cadastrado e enviaremos o código para alterar a senha</h3>
-                    <div className='form-input'>
-                    <Input
-                            type={'text'}
-                            value={recoveryEmail}
-                            onChange={e => {
-                                setRecovertyEmail(e.target.value);
-                            }}
-                        >E-mail</Input></div>                                                                       
-                </Form>              
-                
-            </div>          
-        </div>
-    )
+      <div className="form-box">
+        <Form
+          title={"Recuperação de senha"}
+          buttonValue={"Enviar"}
+          buttonWidth={"90%"}
+          gradient={true}
+          handleSubmit={(event) => {
+            event.preventDefault();
+            if (!recoveryEmail) {
+              alertInvalidFields.current.innerText =
+                "Tipo da conta não informado";
+              return;
+            }
+            
+          }}
+        >
+          <p className="alert-invalid-fields" ref={alertInvalidFields}></p>
+          <h3 className="form-h3">
+            Forneça o E-mail cadastrado e enviaremos o código para alterar a
+            senha
+          </h3>
+          <div className="form-input">
+            <Input
+              type={"text"}
+              value={recoveryEmail}
+              onChange={(e) => {
+                setRecovertyEmail(e.target.value);
+              }}
+            >
+              E-mail
+            </Input>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default PasswordPage;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import {React, useState, useRef} from 'react';
 import Form from "../../components/Form/Form";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import NavBar from "../../components/NavBar/NavBar";
-
+import InfoCardComponent from '../../components/InfoCardComponent/InfoCardComponent';
 import '../../components/Form/Form.css'
 
 const Register = () => {
+
+    const [fullName, setFullName] = useState('');
+    const [cpfNumber, setCpfNumuber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const alertInvalidFields = useRef();
 
     const items = [<Button
         label={'Login'}
@@ -29,13 +37,84 @@ const Register = () => {
                     buttonValue={'Criar conta'}
                     buttonWidth={'60%'}
                     gradient={'true'}
+                    click={() => {}}
+                    handleSubmit={event => {
+                        event.preventDefault();
+                        if(!fullName){
+                            alertInvalidFields.current.innerText = 'Informe seu nome completo';
+                            return;
+                        }
+                        if(!cpfNumber){
+                            alertInvalidFields.current.innerText = 'Informe seu número de CPF';
+                            return;
+                        }
+                        if(!phoneNumber){
+                            alertInvalidFields.current.innerText = 'Informe seu número de celular';
+                            return;
+                        }
+                        if(!email){
+                            alertInvalidFields.current.innerText = 'Informe seu endereço de email';
+                            return;
+                        }
+                        if(!address){
+                            alertInvalidFields.current.innerText = 'Informe seu endereço';
+                            return;
+                        }
+                        if(!password){
+                            alertInvalidFields.current.innerText = 'Informe sua senha';
+                            return;
+                        }
+                        
+                    }}
                 >
-                    <Input>Nome completo</Input>
-                    <Input>CPF</Input>
-                    <Input>Celular</Input>
-                    <Input>E-mail</Input>
-                    <Input>Endereço</Input>
-                    <Input>Senha</Input>
+                    <p className='alert-invalid-fields' ref={alertInvalidFields}></p>
+                    <Input
+                        type={'text'}
+                        value={fullName}
+                        onChange={e => {
+                            setFullName(e.target.value);
+                        }}
+                    >Nome completo</Input>
+
+                    <Input
+                        type={'text'}
+                        value={cpfNumber}
+                        onChange={e => {
+                            setCpfNumuber(e.target.value);
+                        }}
+                    >CPF</Input>
+
+                    <Input
+                        type={'text'}
+                        value={phoneNumber}
+                        onChange={e => {
+                            setPhoneNumber(e.target.value);
+                        }}                 
+                    >Celular</Input>
+
+                    <Input
+                        type={'email'}
+                        value={email}
+                        onChange={e => {
+                            setEmail(e.target.value);
+                        }}
+                    >E-mail</Input>
+
+                    <Input
+                        type={'text'}
+                        value={address}
+                        onChange={e => {
+                            setAddress(e.target.value);
+                        }}                       
+                    >Endereço</Input>
+
+                    <Input
+                        type={'text'}
+                        value={password}
+                        onChange={e => {
+                            setPassword(e.target.value);
+                        }} 
+                    >Senha</Input>
                 </Form>
             </div>
         </>
