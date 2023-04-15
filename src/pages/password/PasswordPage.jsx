46b880843bf5b531a1f78747d87355ac318cf011
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-04-15 18:44:57 -0300

    validad login, passoword and register

--- src/pages/password/PasswordPage.jsx
+++ src/pages/password/PasswordPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import Button from '../../components/Button/Button';
 import Form from '../../components/Form/Form';
@@ -8,10 +8,14 @@ import NavBar from '../../components/NavBar/NavBar';
 import '../../App.css';
 import './PasswordPage.css'
 import { useNavigate } from 'react-router-dom';
+
 const PasswordPage = () => {
     document.title = 'Ímã Pay - Recuperação de senha';
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const errorAlert = useRef();
+
+
     const items = [<Button
         label={'Cadastre-se'}
         width={''}
@@ -40,9 +44,49 @@ const PasswordPage = () => {
                     buttonValue={'Enviar'}
                     buttonWidth={'90%'}
                     gradient={true}
+                    buttonFunction={ 
+                        e => {
+                            e.preventDefault();
+                            const emailInput = e.target.parentElement.parentElement.querySelector('input[name="email"]');
+                            const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+                            const isValidEmail = emailRegex.test(emailInput.value);
+
+                            if(!emailInput.value) {
+                                errorAlert.current.innerText = 'Preencha o campo do email';
+                                return;
+                            }
+
+                            if(!isValidEmail) {
+                                errorAlert.current.innerText = 'Informe um email válido!';
+                                emailInput.classList.add('fildError');
+                                emailInput.parentElement.querySelector('label').classList.add('fildError');
+                                return;
+                            }
+
+                            errorAlert.current.innerText = '';
+
+                            setTimeout(() => {
+                                navigate('/login');
+                            }, 1000);
+                        }
+                    }
                 >
                     <h3 className='form-h3'>Forneça o e-mail cadastrado e enviaremos o código para alterar a senha</h3>
-                    <div className='form-input'><Input>E-mail</Input></div>                                                                       
+
+                    <p className='errorAlert' ref={errorAlert}></p>
+
+                    <div className='form-input'>
+                        <Input
+                            name={'email'}
+                            onChange={
+                                e => {
+                                    e.target.classList.remove('fildError');
+                                    e.target.parentElement.querySelector('label').classList.remove('fildError');
+                                    errorAlert.current.innerText = '';
+                                }
+                            }
+                        >E-mail</Input>
+                        </div>                                                                       
                 </Form>              
                 
             </div>          
