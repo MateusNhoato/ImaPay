d777382a64e8e5eb288cf9f71c29ca7bd03b79ad
victorhugomr <victorhugo_mr@live.com>
2023-04-22 22:09:39 -0300

    integração front/back transaction_history

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -16,27 +16,17 @@ const Table = ({ props }) => {
           <tr>
             <th>{props.head.col1}</th>
             <th>{props.head.col2}</th>
-            <th>{props.head.col3}</th>
-            <th>{props.head.col4}</th>
           </tr>
           <tbody>
             {props.body.map(col => {
-              const signal = col.value4.trim().charAt(0);
-              const cond1 = props.hasCentralTitle && signal == "+";
-              const cond2 = props.hasCentralTitle && signal == "-";
-              const color = cond1 ? "green" : cond2 ? "red" : "";
+              const signal = Number(col.valueTransaction);
+              const color = signal>0 ? "green" : "red";
 
               return (
                 <>
                   <tr>
-                    <td>{col.value1}</td>
-                    <td>{col.value2}</td>
-                    <td>
-                      <span>{col.value3}</span>
-                    </td>
-                    <td>
-                      <span style={{ color }}>{col.value4}</span>
-                    </td>
+                    <td>{col.date}</td>
+                    <span style={{ color }}>{col.valueTransaction}</span>
                   </tr>
                 </>
               );
